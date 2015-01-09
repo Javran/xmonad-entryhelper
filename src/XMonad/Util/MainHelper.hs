efac92d998712eb7766bda3ba4d2eedeb1e37142
Javran Cheng <Javran.c@gmail.com>
2015-01-09 03:18:49 -0500

    skeleton done

---
+++ src/XMonad/Util/MainHelper.hs
@@ -0,0 +1,135 @@
+{-# LANGUAGE FlexibleContexts #-}
+module XMonad.Util.MainHelper where
+
+import XMonad.Core hiding (recompile,config)
+import XMonad.Main
+
+import Data.Functor
+import Control.Applicative
+import Control.Exception.Extensible
+import Control.Monad
+import System.Environment
+import System.Exit
+import System.Posix.Process
+import Graphics.X11.Xlib
+import Graphics.X11.Xlib.Extras
+import System.FilePath
+import System.IO
+import System.Info
+import System.Process
+import System.Directory
+import Data.List ((\\))
+
+data Config = Config
+  { execute :: IO ()
+  , recompileCommand :: IO (FilePath, [String], Maybe FilePath)
+  , upToDateCheck :: IO Bool
+  , binaryPath :: IO FilePath
+  }
+
+defaultUpToDateCheck :: IO Bool
+defaultUpToDateCheck = do
+    dir <- getXMonadDir
+    let binn = "xmonad-"++arch++"-"++os
+        bin  = dir </> binn
+        base = dir </> "xmonad"
+        src  = base ++ ".hs"
+        lib  = dir </> "lib"
+    libTs <- mapM getModTime . Prelude.filter isSource =<< allFiles lib
+    srcT <- getModTime src
+    binT <- getModTime bin
+    return $ not $ any (binT <) (srcT : libTs)
+  where
+    getModTime f = catch (Just <$> getModificationTime f) (\(SomeException _) -> return Nothing)
+    isSource = flip elem [".hs",".lhs",".hsc"] . takeExtension
+    allFiles t = do
+        let prep = map (t</>) . Prelude.filter (`notElem` [".",".."])
+        cs <- prep <$> catch (getDirectoryContents t) (\(SomeException _) -> return [])
+        ds <- filterM doesDirectoryExist cs
+        concat . ((cs \\ ds):) <$> mapM allFiles ds
+
+defaultConfig :: (Read (l Window), LayoutClass l Window) => XConfig l -> Config
+defaultConfig xconf = config
+    where
+      config = Config
+       { execute = xmonad xconf
+       , recompileCommand = do
+             dir <- getXMonadDir
+             binn <- binaryPath config
+             return ( "ghc"
+                    , [ "--make", "xmonad.hs"
+                      , "-i", "-ilib", "-fforce-recomp"
+                      , "-v0"
+                      , "-o", binn]
+                    , Just dir)
+       , upToDateCheck = defaultUpToDateCheck
+       , binaryPath = do
+           dir <- getXMonadDir
+           return $ dir </> "xmonad-"++arch++"-"++os
+       }
+
+withHelper :: (Read (l Window), LayoutClass l Window) => XConfig l -> IO ()
+withHelper xconf = withCustomHelper (defaultConfig xconf)
+
+withCustomHelper :: Config -> IO ()
+withCustomHelper conf = do
+    installSignalHandlers
+    args <- getArgs
+    case args of
+        []                    -> execute conf
+        ("--resume":_)        -> execute conf
+        ["--help"]            -> printUsage >> exitFailure
+        ["--recompile"]       -> recompile conf >>= exitWith
+        ["--replace"]         -> execute conf
+        ["--restart"]         -> sendRestart
+        ["--version"]         -> printVersion False
+        ["--verbose-version"] -> printVersion True
+        _                     -> printUsage >> exitFailure
+
+buildAndLaunch :: Config -> IO ()
+buildAndLaunch c = do
+    upToDate <- upToDateCheck c
+    if upToDate
+      then void launch
+      else do ec <- recompile c
+              case ec of
+                ExitSuccess -> void launch
+                ExitFailure v -> do
+                  putStrLn $ "recompile failed with exitcode " ++ show v
+                  exitFailure
+  where
+    launch = executeFile <$> binaryPath c
+                         <*> pure False
+                         <*> getArgs
+                         <*> pure Nothing
+
+recompile :: Config -> IO ExitCode
+recompile c = do
+    dir <- getXMonadDir
+    let base = dir </> "xmonad"
+        err  = base ++ ".errors"
+    (cmd,args,cmpDir) <- recompileCommand c
+    uninstallSignalHandlers
+    status <- bracket (openFile err WriteMode) hClose $ \h ->
+            waitForProcess =<< runProcess cmd args cmpDir
+                                    Nothing Nothing Nothing (Just h)
+
+    installSignalHandlers
+    return status
+
+printUsage :: IO ()
+printUsage = putStrLn "TODO"
+
+printVersion :: Bool -> IO ()
+printVersion _ = putStrLn "TODO"
+
+sendRestart :: IO ()
+sendRestart = do
+    dpy <- openDisplay ""
+    rw <- rootWindow dpy $ defaultScreen dpy
+    xmonad_restart <- internAtom dpy "XMONAD_RESTART" False
+    allocaXEvent $ \e -> do
+        setEventType e clientMessage
+        setClientMessageEvent e rw xmonad_restart 32 0 currentTime
+        sendEvent dpy rw False structureNotifyMask e
+    sync dpy False
